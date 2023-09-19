66e8c0d628e8c13f5c6db7a654942da6133c15b1
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2023-09-19 02:39:08 +0200

    fix jwt hasura claims

--- netlify/common/jwt.ts
+++ netlify/common/jwt.ts
@@ -5,17 +5,18 @@ const JWT_SECRET = "E0pFuRteHc14J0S6BKO85sIQsNbYGS1f";
 export const HASURA_CLAIMS = "https://hasura.io/jwt/claims";
 export const HASURA_USER_ID = "x-hasura-user-id";
 
-export const signToken = (id: string) =>
-  jwt.sign(
+export const signToken = (id: string) => {
+  return jwt.sign(
     {
-      HASURA_CLAIMS: {
-        "x-hasura-default-role": "admin",
+      [HASURA_CLAIMS]: {
         "x-hasura-allowed-roles": ["admin"],
-        HASURA_USER_ID: id,
+        "x-hasura-default-role": "admin",
+        "x-hasura-user-id": id,
       },
     },
     JWT_SECRET
   );
+};
 
 export const getTokenData = (token:string) => {
   return jwt.verify(token, JWT_SECRET);
